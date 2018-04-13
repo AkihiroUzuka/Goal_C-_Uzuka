1a92c16b69ea0bbc8339e465b5e43f2d27952c56
AkihiroUzuka <akihiro.uzuka@el-ltd.co.jp>
2018-04-13 12:01:32 +0900

    180413

---
+++ AddTax_p.121_list4-3_exception_Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddTax_p._121_exception
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAddTax_Click(object sender, EventArgs e)
+        {
+            int money;
+
+            if (int.TryParse(textBoxMoney.Text, out money) == true)
+            {
+                money = addTax(money);
+                labelAddTax.Text = money + "円";
+            }
+            else
+            {
+                labelAddTax.Text = "入力文字列の形式が正しくありません。";
+            }
+        }
+
+            private int addTax(int m)
+            {
+                const double tax = 0.08;
+                return (int)(m * (1 + tax));
+            }
+        
+    }
+}
