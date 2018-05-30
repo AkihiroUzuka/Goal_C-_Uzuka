ce29c8dc4b3f806532bb75796ca7a6c1403ee937
AkihiroUzuka <akihiro.uzuka@el-ltd.co.jp>
2018-05-30 17:36:50 +0900

    180530

---
+++ list_6-10_DateTimeSample_p.204_Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("今日の日付：" + DateTime.Today);
+            Console.WriteLine("現在の日付と日時：" + DateTime.Now);
+
+            //現在の日付と日時を取得する
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("年 = " + now.Year);
+            Console.WriteLine("月 = " + now.Month);
+            Console.WriteLine("日 = " + now.Day);
+            Console.WriteLine("時 = " + now.Hour);
+            Console.WriteLine("分 = " + now.Minute);
+            Console.WriteLine("秒 = " + now.Second);
+            Console.WriteLine("曜日 = " + now.DayOfWeek);
+
+            Console.Read();
+
+        }
+    }
+}
